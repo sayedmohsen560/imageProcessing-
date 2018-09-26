30e8c0afc900fb15207ee3ea6ef1f82d540c8522
sayedmohsen560 <sayed.mohsen560@gmail.com>
2018-09-26 18:37:06 +0200

    updated threshold file

--- binarisation.py
+++ binarisation.py
@@ -1,15 +1,34 @@
 import cv2
-from PIL import Image
-
-def bin_my_image (image_to_binraize_file):
-    bin_file = Image.open(image_to_binraize_file)  # open colour image
-    bin_file = bin_file.convert('1')  # convert image to black and white
-    bin_file.save('test_result.png')
 
 
 
-def binraztion (rescaled_file):
-    img = cv2.imread(rescaled_file)
-    grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blackandwhite_image = cv2.adaptiveThreshold(grey_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,115,1)
-    cv2.imwrite('test_blackandwhite.jpg',blackandwhite_image)
+def simple_thresholding (image_file):
+    gray_scale = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
+    ret, simple_threshold = cv2.threshold(gray_scale,127,255,cv2.THRESH_BINARY)
+    cv2.imwrite('simple.png',simple_threshold)
+
+
+
+def adaptive_thresholding (image_file):
+    gray_scale = cv2.cvtColor(image_file, cv2.COLOR_BGR2GRAY)
+    th2 = cv2.adaptiveThreshold(gray_scale, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+    th3 = cv2.adaptiveThreshold(gray_scale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    cv2.imwrite('mean.png',th2)
+    cv2.imwrite('gaussen.png', th3)
+
+
+def otsu_thresholding (imge_file):
+    img = cv2.cvtColor(imge_file, cv2.COLOR_BGR2GRAY)
+    ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    cv2.imwrite('results.png',cv2.threshold(img, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1])
+    cv2.imwrite('otsu.png',th2)
+    cv2.imwrite('otsugaussien.png',th3)
+
+
+imgs = cv2.imread('sce.jpg')
+sod = cv2.imread('sod.png')
+simple_thresholding(imgs)
+adaptive_thresholding(imgs)
+otsu_thresholding(imgs)
